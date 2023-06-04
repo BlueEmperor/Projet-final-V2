2f39f2aa374c1bb7c1682c03213402ba6d9b0b17
Loïc <loic_an@protonmail.com>
2023-06-05 00:17:34 +0200

    v3 wip

--- main.py
+++ main.py
@@ -1,38 +1,5 @@
-import pygame
-from sys import exit
-
-pygame.init()
-
-from src.config import Config
-from src.global_state import GlobalState
-from src.status import GameStatus
-from src.game_phases import main_menu_phase, gameplay_phase, end_menu_phase
-from src.draw_cursor import DrawCursor
-
-FramePerSec = pygame.time.Clock()
-
-def update_game_display():
-    pygame.display.flip()
-    FramePerSec.tick(Config.FPS)
-
 def main():
-    while 1:
-        events = pygame.event.get()
-        if GlobalState.GAME_STATE == GameStatus.MAIN_MENU:
-            main_menu_phase(events)
-        elif GlobalState.GAME_STATE == GameStatus.GAMEPLAY:
-            gameplay_phase(events)
-        elif GlobalState.GAME_STATE == GameStatus.GAME_END:
-            end_menu_phase(events)
-
-        DrawCursor(GlobalState.SCREEN)
-        for event in events:
-            if(event.type == pygame.QUIT):
-                pygame.quit()
-                exit()
-        
-        update_game_display()
-        pygame.display.set_caption(str(int(FramePerSec.get_fps())))
+    print("bite")
 
 
 if __name__ == "__main__":
--- src/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    FPS=60
-
-    #Taille de l'écran
-    HEIGHT=800
-    WIDTH=1200
